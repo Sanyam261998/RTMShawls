e3f23e799758d41fe75ac5d4173fc05d23af17e7
Sanyam261998 <sanyamdhawan98@gmail.com>
2025-06-16 11:57:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // assuming index.html is in root
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: "/index.html", // Make sure path is correct
+    },
   },
 });
