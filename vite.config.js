546120748603052032ff640dab5894b5c1052ec0
Sanyam261998 <sanyamdhawan98@gmail.com>
2025-06-16 11:36:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,           // Accept connections from outside (0.0.0.0)
-    port: 5173,           // Ensure this matches your exposed port
-    strictPort: true,
-    watch: {
-      usePolling: true,   // Required for Docker volume watching
+  root: '.', // Important
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
